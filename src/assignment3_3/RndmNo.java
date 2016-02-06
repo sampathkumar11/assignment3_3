e8c68ce1606b06b6680102bad690820ae6a0dc06
sampathkumar11 <sampathkumar11@gmail.com>
2016-02-06 16:28:03 +0530

    Session 3-3
    Finished

--- src/assignment3_3/RndmNo.java
+++ src/assignment3_3/RndmNo.java
@@ -1,22 +1,32 @@
 package assignment3_3;
 
+import java.util.Scanner;
+
 public class RndmNo {
 
 	static String gnrndm(String ino){
-		System.out.println("Hello World! This will generate a random number based on "+ino);
+		//System.out.println("Hello World! This will generate a random number based on "+ino);
+		String random_no;
 		int intno=Integer.parseInt(ino);
 		int intno1=intno/7*3;
-		String random_no=""+intno1;
+		if(intno1<=0){
+			random_no="0";
+			}else{
+				random_no=""+intno1;
+		}
 		return random_no;
 	}
 	
 	
 	public static void main(String[] args) {
-	
-		//System.out.println("Hello World! This will generate a random number");
-		String ino="57";//args[0];
+		Scanner scnr=new Scanner(System.in);
+		
+		System.out.println("Enter a nmber to generate a random number:");
+
+		String ino=scnr.nextLine();//)="57";//args[0];
 	
 		System.out.println("Generated random number : "+gnrndm(ino));
+		scnr.close();
 		
 	}
 
